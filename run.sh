3b83cd3cafa3c6574e1fad411604c7ee41cbcfb6
C-Glick <colton.glick@gmail.com>
2026-05-02 19:14:10 -0500

    Update run script with recommended params

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@ cmake -S ./ -B ./cmake-build-debug -D CMAKE_BUILD_TYPE=Debug
 echo "===== Compiling with cmake... ====="
 cmake --build ./cmake-build-debug --config Debug --target CUDA_MR_Optical_Tracking -j $(nproc)
 
-echo "—————===== Running the executable... =====—————"
-./cmake-build-debug/CUDA_MR_Optical_Tracking
+echo "—————===== Running the executable with recommended settings... =====—————"
+./cmake-build-debug/CUDA_MR_Optical_Tracking --help
+./cmake-build-debug/CUDA_MR_Optical_Tracking --calibration=./camera_calibration --video=./GlickArucoSampleVideo.mp4
